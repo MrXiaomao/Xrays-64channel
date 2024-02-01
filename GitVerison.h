c421ef603e291c2b0557e4a374f40987d3cab703
1207402640 <MrXiaomao>
2024-02-01 22:22:34 +0800

    删除多余图标

--- GitVerison.h
+++ GitVerison.h
@@ -1,4 +1,4 @@
 #ifndef _GITVER_H
 #define _GITVER_H
-#define GIT_VER "Sun Jan 28 21:36:26 2024 +0800 hash:e3e2d70 modified"
+#define GIT_VER "Thu Feb 1 22:19:53 2024 +0800 hash:1daf141 modified"
 #endif
